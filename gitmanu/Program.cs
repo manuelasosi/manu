1f95daa0904f266762a99317f32738d03235b9d7
manuelasosi <manuelasouzasilva687@gmail.com>
2023-10-05 15:48:43 -0400

    segundo commit

--- gitmanu/Program.cs
+++ gitmanu/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class oi
+{
+    static void Main(string[] args)
+    {
+        Console.Write(" Digite um numero:");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outro numero:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = num + num2;
+        Console.WriteLine($" A soma dos numeros é {result}");
+
+        if (num2 > num)
+        {
+            Console.WriteLine($"O numero {num2} é maior");
+        }
+
+        Console.ReadKey();
+    }
+}
